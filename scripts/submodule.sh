01ee4bfd23c3c4ffe579348199abc1da97c94046
Adam <mindreeper2420@gmail.com>
2020-02-12 09:32:09 -0500

    Add various development scripts

---
+++ scripts/submodule.sh
@@ -0,0 +1,31 @@
+if [ -z "$(ls -A developers.redhat.com)" ]; then
+  echo "Submodule is empty/missing"
+  sleep 2
+  echo "Initialize Git Submodule"
+  git submodule init
+  sleep 2
+
+  git submodule update --remote developers.redhat.com
+  sleep 2
+
+  echo "Change to Submodule Directory"
+  cd developers.redhat.com
+  sleep 2
+
+  echo "Update Submodule"
+  git submodule update
+  sleep 2
+
+  echo "Install Submodule Dependencies"
+  sleep 2
+  npm install
+  sleep 2
+
+  echo "Return to HEAD"
+  cd ../../../../../../../../../
+
+else
+   echo "Submodule exists: Updating"
+   sleep 2
+   git submodule update --remote developers.redhat.com
+fi
